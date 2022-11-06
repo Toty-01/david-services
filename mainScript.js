767e584938b6efb8122df1bec2360190071c9ea8
Toty-01 <caucheanthony@gmail.com>
2022-11-06 17:20:19 +0100

    pop up slider responsive integrée

--- mainScript.js
+++ mainScript.js
@@ -36,3 +36,76 @@ function copy(copyId){
   }, 1000);
 }
 
+var imageUrls   = ['url("./douches-photo/sdo(3).jpg")','url("./douches-photo/sdo(2).jpg")','url("./douches-photo/sdo(5).jpg")', 
+                   'url("./douches-photo/sdo(4).jpg")', 'url("./douches-photo/sdo(8).jpg")', 'url("./douches-photo/sdo(7).jpg")', 
+                   'url("./douches-photo/sdo(10).jpg")', 'url("./douches-photo/sdo(12).jpg")','url("./douches-photo/sdo(24).jpg")',
+                   'url("./douches-photo/sdo(23).jpg")', 'url("./douches-photo/sdo(26).jpg")', 'url("./douches-photo/sdo(25).jpg")']
+
+var imageNumber = imageUrls.length
+
+function imagePop(id){
+
+    document.getElementById('imagePopId').style.display = 'block'
+    document.getElementsByTagName('body')[0].style.overflowY = 'hidden'
+
+    var imageName = document.getElementById(id + '-image').style.backgroundImage
+
+    var imageIndex = 0
+
+    imageIndex = imageUrls.indexOf(imageName)
+    
+    document.getElementsByClassName('imageContain')[0].style.animation = 'he 800ms forwards'
+
+    document.getElementsByClassName('imageContain')[0].style.backgroundImage = imageUrls[imageIndex]
+}
+function imageChange(id){
+
+    var imageName = document.getElementById(id).style.backgroundImage
+
+    var imageIndex = 0
+
+    imageIndex = imageUrls.indexOf(imageName)
+
+    if(imageIndex >= imageNumber - 1)
+    imageIndex = 0
+    else
+    imageIndex++
+
+    document.getElementById(id).style.backgroundImage = imageUrls[imageIndex]
+}
+function imageMoveLeft(){
+
+    var imageName = document.getElementById('imageContainId').style.backgroundImage
+
+    var imageIndex = 0
+
+    imageIndex = imageUrls.indexOf(imageName)
+
+    if(imageIndex <= 0)
+    imageIndex = imageNumber - 1
+    else
+    imageIndex--
+
+    document.getElementById('imageContainId').style.backgroundImage = imageUrls[imageIndex];
+}
+function imageMoveRight(){
+
+    var imageName = document.getElementById('imageContainId').style.backgroundImage
+
+    var imageIndex = 0
+
+    imageIndex = imageUrls.indexOf(imageName)
+
+    if(imageIndex >= imageNumber - 1)
+    imageIndex = 0
+    else
+    imageIndex++
+
+    document.getElementById('imageContainId').style.backgroundImage = imageUrls[imageIndex]
+}
+function imagePopNone(){
+
+    document.getElementsByClassName('imagePop')[0].style.display = 'none'
+    document.getElementsByTagName('body')[0].style.overflowY = 'scroll'
+
+}
